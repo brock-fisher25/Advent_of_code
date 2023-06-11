13c9f00137d331f802de54120b6c3bd6e30a42d1
brock-fisher25 <brock_fisher25@yahoo.com>
2023-06-11 14:31:19 -0600

    fixed day 6

--- 2015/python/Day_6/Day_6.py
+++ 2015/python/Day_6/Day_6.py
@@ -3,12 +3,12 @@ def read_text_file():
         return file.readlines()
     
 def toggle(map, first_instruction, second_instruction, state):
-    first_set = first_instruction.split(',')
-    second_set = second_instruction.split(',')
-    first_set = [eval(i) for i in first_set]
-    second_set = [eval(i) for i in second_set]
-    for i in range(first_set[0], second_set[0] + 1):
-        for j in range(first_set[1], second_set[1] + 1):
+    first_set_coordinates = first_instruction.split(',')
+    second_set_coordinates = second_instruction.split(',')
+    first_set_coordinates = [eval(i) for i in first_set_coordinates]
+    second_set_coordinates = [eval(i) for i in second_set_coordinates]
+    for i in range(first_set_coordinates[0], second_set_coordinates[0] + 1):
+        for j in range(first_set_coordinates[1], second_set_coordinates[1] + 1):
             if state == 'off':
                 map[i][j] = 0
             elif state == 'on':
@@ -18,9 +18,8 @@ def toggle(map, first_instruction, second_instruction, state):
     return map
 
 def first_part():
-    map = [[0] * 1000] *1000
+    map = [[0 for i in range(1000)] for row in range(1000)]
     instructions = read_text_file()
-    counter = 0
     for instruction in instructions:
         instruction_list = instruction.split(' ')
         if instruction_list[1] == 'off':
@@ -29,9 +28,6 @@ def first_part():
             map = toggle(map, instruction_list[2], instruction_list[-1], 'on')
         else:
             map = toggle(map, instruction_list[1], instruction_list[-1], 'toggle')
-        counter += 1
-        if counter > 9:
-            break
     total_lights = 0
     for i in map:
         for j in i:
@@ -39,9 +35,35 @@ def first_part():
                 total_lights += 1
     return total_lights
 
+def brightness(map, first_instruction, second_instruction, brightness_change):
+    first_set_coordinates = first_instruction.split(',')
+    second_set_coordinates = second_instruction.split(',')
+    first_set_coordinates = [eval(i) for i in first_set_coordinates]
+    second_set_coordinates = [eval(i) for i in second_set_coordinates]
+    for i in range(first_set_coordinates[0], second_set_coordinates[0] + 1):
+        for j in range(first_set_coordinates[1], second_set_coordinates[1] + 1):
+            map[i][j] += brightness_change
+            if map[i][j] < 0:
+                map[i][j] = 0
+    return map
+
 
 def second_part():
-    return
+    map = [[0 for i in range(1000)] for row in range(1000)]
+    instructions = read_text_file()
+    for instruction in instructions:
+        instruction_list = instruction.split(' ')
+        if instruction_list[1] == 'off':
+            map = brightness(map, instruction_list[2], instruction_list[-1], -1)
+        elif instruction_list[1] == 'on':
+            map = brightness(map, instruction_list[2], instruction_list[-1], 1)
+        else:
+            map = brightness(map, instruction_list[1], instruction_list[-1], 2)
+    total_brightness = 0
+    for i in map:
+        for j in i:
+            total_brightness += j
+    return total_brightness
 
 if __name__ == "__main__":
     print(first_part()) #returns 322000 is too low
