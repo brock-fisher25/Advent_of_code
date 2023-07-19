c8e1d0c6dde91bc8611227179843fb640090808e
brock-fisher25 <brock_fisher25@yahoo.com>
2023-07-18 19:26:38 -0600

    finish day 8

--- 2015/python/Day_8/Day_8.py
+++ 2015/python/Day_8/Day_8.py
@@ -3,33 +3,25 @@ def read_text_file():
     with open('Day_8_input.txt') as file:
         return file.readlines()
 
-def manipulate_string(string):
-    string = string.replace('\\\\', '\\')
-    string = string.replace('\"', '"')
-    print(string)
-    return string
-    
-
-
 def first_part():
     list_of_strings = read_text_file()
     num_chars_literal = 0
-    count = 0
     num_chars_mem = 0
     for string in list_of_strings:
         s1 = string.strip() # get rid of \n char
         num_chars_literal += (len(s1))
-        print(s1)
-        mem_string = manipulate_string(s1)
-        num_chars_mem += (len(s1) - len(mem_string))
-        if count == 5:
-            exit()
-        count += 1
+        num_chars_mem += len(eval(s1))
     return num_chars_literal - num_chars_mem
 
 def second_part():
-    return
+    list_of_strings = read_text_file()
+
+    sum = 0
+    for string in list_of_strings:
+        sum += 2 + string.count('\\') + string.count('"')
+
+    return sum
 
 if __name__ == "__main__":
-    print(first_part()) #returns 1362 is too high
-    print(second_part()) #returns
+    print(first_part()) #returns 1350
+    print(second_part()) #returns 2085
