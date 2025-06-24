87f53ee5f263fa63468f9fe903e7492773b717b7
brock-fisher25 <94400152+brock-fisher25@users.noreply.github.com>
2025-06-24 09:29:45 -0600

    Update Day_3.py

--- 2024/Python/Day 3/Day_3.py
+++ 2024/Python/Day 3/Day_3.py
@@ -3,8 +3,8 @@ import os
 os.chdir("C:\Documents\Personal\Github\Advent-of-code")
 
 def read_file():
-    file = open("input.txt", 'r')
-    return file
+    with open("input.txt", "r") as file:
+        return file.read()
 
 def process_string(string):
     x, y = 0, 0
@@ -31,51 +31,52 @@ def process_string(string):
 
 
 def solution_one():
-    file = read_file()
+    all_instructions = read_file()
     total = 0
-    for line in file:
-        temp = list(line.split('mul('))
-        for string in temp:
-            # if digit is in first slot (0), process string
-            if len(string) == 0:
-                continue
-            if string[0].isdigit():
-                total += process_string(string)
+    temp = list(all_instructions.split('mul('))
+    for string in temp:
+        # if digit is in first slot (0), process string
+        if len(string) == 0:
+            continue
+        if string[0].isdigit():
+            total += process_string(string)
 
     return total
 
 def solution_two():
     file = read_file()
+    all_instructions = ''
+    for line in file:
+        all_instructions += line
     total = 0
     process_initial = True
-    for line in file:
-        # split string on each valid don't instruction to break up instructions
-        temp = list(line.split("don't()"))
-        for string in temp:
-            # always count first batch of instructions
-            if process_initial:
-                process_initial = False
-                initial_strings = list(temp[0].split('mul('))
-                for initial in initial_strings:
-                    if len(initial) == 0:
+    # split string on each valid don't instruction to break up instructions
+    temp = list(all_instructions.split("don't()"))
+    for string in temp:
+        # always count first batch of instructions
+        if process_initial:
+            process_initial = False
+            initial_strings = list(temp[0].split('mul('))
+            for initial in initial_strings:
+                if len(initial) == 0:
+                    continue
+                if initial[0].isdigit():
+                    total += process_string(initial)
+            continue
+        # split each don't instruction on every do instruction, to get instructions that should be counted
+        if 'do()' in string:
+            temp_2 = list(string.split('do()'))
+            # skip first one as those instructions shouldn't be followed as they proceed the latest don't
+            for i in range(1, len(temp_2)):
+                process_strings = list(temp_2[i].split('mul('))
+                for substring in process_strings:
+                    if len(substring) == 0:
                         continue
-                    if initial[0].isdigit():
-                        total += process_string(initial)
-                continue
-            # split each don't instruction on every do instruction, to get instructions that should be counted
-            if 'do()' in string:
-                temp_2 = list(string.split('do()'))
-                # skip first one as those instructions shouldn't be followed as they proceed the latest don't
-                for i in range(1, len(temp_2)):
-                    process_strings = list(temp_2[i].split('mul('))
-                    for substring in process_strings:
-                        if len(substring) == 0:
-                            continue
-                        if substring[0].isdigit():
-                            total += process_string(substring)
+                    if substring[0].isdigit():
+                        total += process_string(substring)
 
     return total
-    
+
 if __name__ == '__main__':
     answer_one = solution_one()
     answer_two = solution_two()
