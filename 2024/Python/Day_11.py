6231fa8e7324b65ec3ced9012dba5e359224e550
brock-fisher25 <brock_fisher25@yahoo.com>
2025-07-18 14:59:41 -0600

    finish day 11

--- 2024/Python/Day_11.py
+++ 2024/Python/Day_11.py
@@ -1,5 +1,5 @@
 def read_file():
-    file = open("input.txt", "r")
+    file = open("input_day_11.txt", "r")
     return file.read().split()
 
 def process_stone(stone, stone_dict):
@@ -16,28 +16,26 @@ def process_stone(stone, stone_dict):
         result = (str(int(stone) * 2024), None)
     stone_dict[stone] = result
     return result
-# [occurences of stone, stone_count]
+
 def process_stones(stone, stone_dict, stone_count, blinks):
+    original_stone = stone
     if (stone, blinks) in stone_dict:
-        stone_dict[(stone, blinks)][0] += 1
-        return stone_dict, stone_count
+        return stone_dict[(stone, blinks)]
     if blinks == 0:
-        return stone_dict, (stone_count + 1)
+        return 1
     
-    blinks -= 1
     if stone == '0':
         stone = '1'
-        stone_dict, stone_count = process_stones(stone, stone_dict, stone_count, blinks)
+        stone_count = process_stones(stone, stone_dict, stone_count, blinks - 1)
     elif len(stone) % 2 == 0:
         first_stone = stone[:len(stone)//2]
         second_stone = str(int(stone[len(stone)//2:]))
-        stone_dict, stone_count = process_stones(first_stone, stone_dict, stone_count, blinks)
-        stone_dict, stone_count = process_stones(second_stone, stone_dict, stone_count, blinks)
+        stone_count = process_stones(first_stone, stone_dict, stone_count, blinks - 1) + process_stones(second_stone, stone_dict, stone_count, blinks - 1)
     else:
         stone = str(int(stone) * 2024)
-        stone_dict, stone_count = process_stones(stone, stone_dict, stone_count, blinks)
-    stone_dict[(stone, blinks)] = [1, stone_count]
-    return stone_dict, stone_count
+        stone_count = process_stones(stone, stone_dict, stone_count, blinks - 1)
+    stone_dict[(original_stone, blinks)] = stone_count
+    return stone_count
 
 # def solution_one():
 #     stones = read_file()
@@ -57,28 +55,27 @@ def process_stones(stone, stone_dict, stone_count, blinks):
 def solution_one():
     stones = read_file()
     stone_dict = {}
-    count = 0
+    blinks = 25
     for stone in stones:
-        stone_dict, count = process_stones(stone, stone_dict, 0, 2)
+        process_stones(stone, stone_dict, 0, blinks)
     total = 0
-    for key, value in stone_dict.items():
-        total += (int(value[0]) * int(value[1]))
+    for stone in stones:
+        total += int(stone_dict[stone, blinks])
     return total
 
 def solution_two():
-    return
     stones = read_file()
     stone_dict = {}
-    count = 0
+    blinks = 75
     for stone in stones:
-        stone_dict, count = process_stones(stone, stone_dict, 0, 75)
+        process_stones(stone, stone_dict, 0, blinks)
     total = 0
-    for key, value in stone_dict.items():
-        total += (int(value[0]) * int(value[2]))
+    for stone in stones:
+        total += int(stone_dict[stone, blinks])
     return total
 
 if __name__ == '__main__':
-    answer_one = solution_one() #239714
+    answer_one = solution_one()
     answer_two = solution_two()
     print(answer_one)
     print(answer_two)
