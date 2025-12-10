4e104572d2a726fc152d91c1235cfb014cc977c2
brock-fisher25 <94400152+brock-fisher25@users.noreply.github.com>
2025-12-10 16:41:42 -0700

    Update Day_9.py

--- 2025/Python/Day_9.py
+++ 2025/Python/Day_9.py
@@ -93,39 +93,90 @@ def solution_two():
         coord1 = coordinates[i]
         for j in range(i+1, len(coordinates)):
             coord2 = coordinates[j]
-            l_edge = True
-            r_edge = True
-            t_edge = True
-            b_edge = True
+            l_edge, r_edge, t_edge, b_edge = True, True, True, True
+            tl_corner, tr_corner, bl_corner, br_corner  = [], [], [], []
+            # figure out each corner of rectangle
+            if coord1[0] < coord2[0] and coord1[1] < coord2[1]:
+                tl_corner = coord1
+                br_corner = coord2
+                tr_corner = [coord1[0], coord2[1]]
+                bl_corner = [coord2[0], coord1[1]]
+            elif coord1[0] > coord2[0] and coord1[1] < coord2[1]:
+                bl_corner = coord1
+                tr_corner = coord2
+                tl_corner = [coord2[0], coord1[1]]
+                br_corner = [coord1[0], coord2[1]]
+            elif coord1[0] > coord2[0] and coord1[1] > coord2[1]:
+                br_corner = coord1
+                tl_corner = coord2
+                tr_corner = [coord2[0], coord1[1]]
+                bl_corner = [coord1[0], coord2[1]]
+            elif coord1[0] < coord2[0] and coord1[1] > coord2[1]:
+                tr_corner = coord1
+                bl_corner = coord2
+                tl_corner = [coord1[0], coord2[1]]
+                br_corner = [coord2[0], coord1[1]]
+            elif coord1[0] == coord2[0]:
+                if coord1[1] > coord2[1]:
+                    tl_corner = coord2
+                    tr_corner = coord1
+                else:
+                    tl_corner = coord1
+                    tr_corner = coord2
+            elif coord1[1] == coord2[1]:
+                if coord1[0] > coord2[0]:
+                    tl_corner = coord2
+                    bl_corner = coord1
+                else:
+                    tl_corner = coord1
+                    bl_corner = coord2
             # check each edge of rectangle to ensure it is in shape
             # check to see if rectangle is a single horizontal line
             if coord1[0] == coord2[0]:
-                t_edge = check_edge(valid_row_ranges, valid_col_ranges, coord1, coord2)
-                b_edge = check_edge(valid_row_ranges, valid_col_ranges, coord1, coord2)
+                t_edge = check_horiz_edge(valid_row_ranges, valid_col_ranges, tl_corner, tr_corner)
+                b_edge = True
                 l_edge = True
                 r_edge = True
             # check to see if rectangle is a single vertical line
             elif coord1[1] == coord2[1]:
-                l_edge = check_edge(valid_row_ranges, valid_col_ranges, coord1, coord2)
-                r_edge = check_edge(valid_row_ranges, valid_col_ranges, coord1, coord2)
+                l_edge = check_vert_edge(valid_row_ranges, valid_col_ranges, tl_corner, bl_corner)
+                r_edge = True
                 b_edge = True
                 t_edge = True
             else:
-                t_edge = check_edge(valid_row_ranges, valid_col_ranges, coord1, coord2)
-                b_edge = check_edge(valid_row_ranges, valid_col_ranges, coord1, coord2)
-                l_edge = check_edge(valid_row_ranges, valid_col_ranges, coord1, coord2)
-                r_edge = check_edge(valid_row_ranges, valid_col_ranges, coord1, coord2)
+                t_edge = check_horiz_edge(valid_row_ranges, valid_col_ranges, tl_corner, tr_corner)
+                b_edge = check_horiz_edge(valid_row_ranges, valid_col_ranges, bl_corner, br_corner)
+                l_edge = check_vert_edge(valid_row_ranges, valid_col_ranges, tl_corner, bl_corner)
+                r_edge = check_vert_edge(valid_row_ranges, valid_col_ranges, tr_corner, br_corner)
             if l_edge and r_edge and t_edge and b_edge:
                 new_area = calculate_area(coord1, coord2)
                 if area < new_area:
                     area = new_area
-    return sum
+    return area
 
-def check_edge(valid_row_ranges, valid_col_ranges, coord1, coord2):
+def check_horiz_edge(valid_row_ranges, valid_col_ranges, coord1, coord2):
+    coords_to_check = []
+    for i in range(coord1[1], coord2[1] + 1):
+        coords_to_check.append([coord1[0], i])
+    ranges = valid_row_ranges[coord1[0]]
+    for coord in coords_to_check:
+        for x in ranges:
+            if coord[1] < x[0] or coord[1] > x[1]:
+                return False
     return True
 
+def check_vert_edge(valid_row_ranges, valid_col_ranges, coord1, coord2):
+    coords_to_check = []
+    for i in range(coord1[0], coord2[0] + 1):
+        coords_to_check.append([i, coord1[1]])
+    ranges = valid_col_ranges[coord1[1]]
+    for coord in coords_to_check:
+        for x in ranges:
+            if coord[0] < x[0] or coord[0] > x[1]:
+                return False
+    return True
 
-
+# confirmed that there are only two coordinates in each column and in each row
 if __name__ == '__main__':
     answer_one = solution_one()
     print(answer_one)
